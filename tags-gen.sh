3b8a3beaf43a7b93e88b6415f08b46b07e9afa12
zhouchengming <eric.zcm@cainiao.com>
2018-12-10 17:09:21 +0800

    cgi enabled in apache2

--- tags-gen.sh
+++ tags-gen.sh
@@ -7,10 +7,6 @@ then
 fi
 
 cd $1 || { echo "chdir $1 failed" && exit; }
-if ! $(which gtags)
-then
-  apt-get install global || exit
-fi
 gtags
 #htags --auto-completion -fFDnvxhost $2
 htags -fFDnvhost $2
